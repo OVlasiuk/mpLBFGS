6ea2ebacfc7716910e99ed7db65c1aceee592206
OVlasiuk <oleksandr.vlasiuk@vanderbilt.edu>
2018-12-10 00:33:56 +0200

    Add input

--- example-pframe.cpp
+++ example-pframe.cpp
@@ -1,8 +1,8 @@
 // pframe energy optimization (unconstrained version)
 #include <iostream>
-#include <eigen3/Eigen/Core>
 #include "include/mpreal.h"
 #include "include/LBFGS.h"
+#include <eigen3/Eigen/Core>
 #include <eigen3/unsupported/Eigen/MPRealSupport>
 
 using namespace mpfr;
@@ -26,8 +26,8 @@ private:
     VectorXmp tempp;  //stores pth powers of norms
     VectorXmp temp2;  //stores squares of norms
     mpreal p;
-    mpreal f(mpreal t) { return mpfr::pow(mpfr::abs(t), p); };
-    mpreal fprime(mpreal t) { return p * t * mpfr::pow(mpfr::abs(t), p-2); };
+    mpreal f(mpreal t) { return mpfr::pow(mpfr::abs(t), p); };  // p-th power of the absolute value
+    mpreal fprime(mpreal t) { return p * t * mpfr::pow(mpfr::abs(t), p-2); };   // derivative of f
 public:
     Pframe(unsigned n_, unsigned dim_, mpreal p_) : n{n_}, dim{dim_}, p{p_} {
         tempp = VectorXmp::Zero(n);
@@ -35,24 +35,25 @@ public:
     }
     mpreal operator()(const VectorXmp& x, VectorXmp& grad)
     {
-        // store norms in the data member
+        // Store norms in the data members
         for(int i = 0; i < n; i++)
-        {
-            temp2[i] = x.segment(i*dim, dim).dot(x.segment(i*dim, dim)); // compare with a loop
-            tempp[i] = mpfr::pow(temp2[i], p/2.0); 
+        { 
+            temp2[i] = x.segment(i*dim, dim).dot(x.segment(i*dim, dim));    // squared norms of the configuration 
+            tempp[i] = mpfr::pow(temp2[i], p/2.0);  // p-th powers of norms of the configuration
         }
-        // energy
-        mpreal fx {0.0}; 
+        // Energy
+        mpreal fx {n/2.0}; 
         for(int i = 0; i < n; i++)
             for (int j = 0; j < i; j++)
             {
                 fx += f(x.segment(j*dim, dim).dot(x.segment(i*dim, dim))) /tempp[i] /tempp[j];
             }
-        fx *= 2.0;
-        // gradient
-        for (int l = 0; l < dim; l++)
+        fx *= 2.0 / n / n;
+        // Gradient
+        grad = VectorXmp::Zero(n*dim);
+        for (int l = 0; l < dim; l++)   // iterate over coordinates
         {
-            for(int i = 0; i < n; i++)
+            for(int i = 0; i < n; i++)  // iterate over vectors
             {
                 mpreal gr {0};
                 for (int j = 0; j<n; j++)
@@ -62,47 +63,60 @@ public:
                             fprime(prod) * x(j*dim+l) - p * x(i*dim+l) * f(prod) / temp2[i]
                           ) / tempp[i] / tempp[j]; 
                 }
-                grad(i*dim+l) = 2 * gr;
+                grad(i*dim+l) = 2 * gr / n / n; // "2" to account for i-th column and i-th row
             }
         }
         return fx;
     }
 };
 
-int main()
+int main(int argc, char *argv[])
 {
-    const unsigned n  {56};
-    const unsigned dim  {7};
+    // Initialize scalar parameters
+    unsigned n  {12};
+    unsigned dim  {3};
     mpreal p {3};
+    std::string epsilon {"20"};
+    mpreal fx, gn;
+    switch (argc) {
+        case 5 ... INT_MAX: // only gcc and clang-compatible
+            epsilon = argv[4];
+        case 4: // only gcc and clang-compatible
+            p = atof(argv[3]);
+        case 3:
+            dim = atoi(argv[2]);
+        case 2:
+            n = atoi(argv[1]);
+    }
+    std::cout << "n = " << n <<  std::endl;
+    std::cout << "dim = " << dim <<  std::endl;
+    std::cout << "p = " << p <<  std::endl;
+    std::cout << "-log10(epsilon) = " << epsilon <<  std::endl;
+
+    // Initialize vector parameters
+    VectorX y(n*dim);
+    VectorXmp x(n*dim);
+
+    // Initialize system parameters
     mpreal::set_default_prec(mpfr::digits2bits(100));
-    VectorXmp grad = VectorXmp::Zero(n*dim);
-    std::cout.precision(8);
     std::srand((unsigned int) time(0));
 
+    // Initialize the solver class
     LBFGSParam<mpreal> param;
-    param.epsilon = mpreal(1e-10);
-    LBFGSSolver<mpreal> solver(param);
-
+    param.epsilon = mpreal("1e-"+epsilon);
+    LBFGSSolver<mpreal> solver(param); 
     Pframe fun(n, dim, p);
 
-    // std::cout << "mpfr random\n" << mpfr::random((unsigned int) time(0)) << std::endl;
+    // std::cout << "mpfr random\n" << mpfr::random((unsigned int) time(0)) << std::endl; 
+    y = VectorX::Random(n*dim); // random starting config
+    x += y.cast<mpreal>();  // TODO: VectorXmp::Random does not work?  
 
+    int niter = solver.minimize(fun, x, fx, gn);
+    //
 
-    // VectorXmp dz = VectorXmp(n*dim);
-    // y << 1, 3, 0, 1 ; 
-    // dz << 1e-12, 0, 0, 0; 
-
-    // VectorXmp x = VectorXmp::Random(n*dim);
-    VectorX y = VectorX::Random(n*dim);
-    VectorXmp x = y.cast<mpreal>(); 
-    mpreal fx;
-    int niter = solver.minimize(fun, x, fx);
-
-    Eigen::Map<MatrixXmp> M(x.data(), n,dim);
-
-    // .normalize does not work with mpreals?
+    Eigen::Map<MatrixXmp> M(x.data(), n,dim); 
     VectorXmp temp2 = VectorXmp::Zero(n);
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++)  // TODO: this->normalize does not work with mpreals?
     {
         temp2[i] = mpfr::sqrt(x.segment(i*dim, dim).dot(x.segment(i*dim, dim))); 
     }
@@ -110,15 +124,11 @@ int main()
         M.row(i) /= temp2[i];
     //
     
-    // std::cout << "M:" << std::endl << M << std::endl; 
-    //
-    // VectorXmp G = M.rowwise().norm(); 
-    // std::cout << "x:" << std::endl << x << std::endl;
-    //
-    std::cout << "Gram matrix = \n"  <<  M*M.transpose() << std::endl;
+    // std::cout << "Gram matrix = \n"  <<  M*M.transpose() << std::endl;
     std::cout << niter << " iterations" << std::endl;
+    std::cout.precision(24);
     std::cout << "f(x) = " << fx << std::endl;
-    std::cout <<  "Gradient norm \n" << grad.norm()  << std::endl; 
+    std::cout <<  "Gradient norm \n" << gn  << std::endl; 
 
     return 0;
 }

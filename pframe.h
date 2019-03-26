0e94377f51d4e64ce56fe4112e3116b3ebca6a63
OVlasiuk <oleksandr.vlasiuk@gmail.com>
2019-03-26 18:06:05 -0400

    Add unweighted pframe header

---
+++ pframe.h
@@ -0,0 +1,85 @@
+#include "include/mpreal.h"
+#include "include/LBFGS.h"
+#include <eigen3/Eigen/Core>
+#include <eigen3/unsupported/Eigen/MPRealSupport>
+
+using Eigen::Array;
+using Eigen::Matrix;
+using Eigen::Dynamic;
+using Eigen::RowMajor;
+
+typedef Matrix<mpreal,Dynamic,Dynamic, RowMajor>  MatrixXmp;
+typedef Matrix<mpreal,Dynamic,1>        VectorXmp;
+typedef Matrix<double,Dynamic,1>        VectorX;
+typedef Array<mpreal, Dynamic, Dynamic> ArrayXmp;
+
+class Pframe
+{
+private:
+    const unsigned n;
+    const unsigned dim;
+    VectorXmp tempp;  //stores pth powers of norms
+    VectorXmp temp2;  //stores squares of norms
+    mpreal p;
+    mpreal f(mpreal t) { return mpfr::pow(mpfr::abs(t), p); };  // p-th power of the absolute value
+    mpreal fprime(mpreal t) { return p * t * mpfr::pow(mpfr::abs(t), p-2); };   // derivative of f
+public:
+    Pframe(unsigned n_, unsigned dim_, mpreal p_) : n{n_}, dim{dim_}, p{p_} {
+        tempp = VectorXmp::Zero(n);
+        temp2 = VectorXmp::Zero(n);
+    }
+    mpreal operator()(const VectorXmp& x, VectorXmp& grad)
+    {
+        // Store norms in the data members
+        for(int i = 0; i < n; i++)
+        { 
+            temp2[i] = x.segment(i*dim, dim).dot(x.segment(i*dim, dim));    // squared norms of the configuration 
+            tempp[i] = mpfr::pow(temp2[i], p/mpreal("2.0"));  // p-th powers of norms of the configuration
+        }
+        // Energy
+        mpreal fx {n/mpreal("2.0")}, jterms; 
+        for(int i = 0; i < n; i++)
+        { 
+            jterms = 0.0;
+            for (int j = 0; j < i; j++)
+            {
+                jterms += f(x.segment(j*dim, dim).dot(x.segment(i*dim, dim))) /tempp[j];
+            }
+            jterms /= tempp[i];
+            fx += jterms;
+        }
+        fx *= mpreal("2.0") / n / n;
+        // Gradient
+        grad = VectorXmp::Zero(n*dim);
+        for (int l = 0; l < dim; l++)   // iterate over coordinates
+        {
+            for(int i = 0; i < n; i++)  // iterate over vectors
+            {
+                mpreal gr  {"0"};
+                for (int j = 0; j<n; j++)
+                {
+                    mpreal prod = x.segment(j*dim, dim).dot(x.segment(i*dim, dim));
+                    gr += (
+                            fprime(prod) * x(j*dim+l) - p * x(i*dim+l) * f(prod) / temp2[i]
+                          ) / tempp[i] / tempp[j]; 
+                }
+                grad(i*dim+l) = mpreal("2") * gr / n / n; // "2" to account for i-th column and i-th row
+            }
+        }
+        return fx;
+    }
+
+    void testgrad(VectorXmp x0)
+    {
+        // Test gradient
+        VectorXmp   h(x0.size()), z(x0.size()), y0(x0.size()), y1(x0.size()); 
+        mpreal hterm = 1e-12;
+        for (int j = 0; j < x0.size(); j++)
+        {
+            h[j] = hterm;
+            z[j] = (this->operator()(x0+h, y1) - this->operator()(x0, y0))/hterm;
+            h[j] = 0.0;
+        }
+        std::cout << (z - y0).norm() << std::endl;
+    }
+};
